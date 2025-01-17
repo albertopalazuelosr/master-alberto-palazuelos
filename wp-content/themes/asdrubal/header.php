3624e569ef7cd8ce244f2c3b1fafbc0b78a88244
albertopalazuelosr <albertopalazuelosr@gmail.com>
2025-01-17 21:39:41 +0100

    headings + footer modificados

---
+++ wp-content/themes/asdrubal/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="/css/fonts.css">
+    <link rel="stylesheet" href="/css/estilo.css">
+</head>
+<body>
+    <header>
+      <nav>
+        <ul>
+            <li><a href="/">Inicio</a></li>
+            <li><a href="/sobre-mi">Sobre mí</a></li>
+            <li><a href="/contacto">Contacto</a></li>
+            <li><a href="/carpeta/archivo-carpeta">Archivo dentro de carpeta</a></li>
+            <li><a href="https://www.w3schools.com/" target="_blank">Más información</a></li>
+            <li><a href="/csss">Explicación CSS</a></li>
+            <li><a href="/redireccion">Redirección</a></li>
+            <li><a href="/redireccion-js">Redirección JS</a></li>
+        </ul>
+     </nav>
+    </header>
--- wp-content/themes/asdrubal/page.php
+++ wp-content/themes/asdrubal/page.php
@@ -1,1 +1,10 @@
-<?php echo "<h1>Hola hola hola</h1>" ;?> 
+<?php
+// Códigos de utilidad// Saca la ruta hasta el tema
+echo get_template_directory_uri();// Saca el lang del idioma configurado en WordPress
+language_attributes();// Actívame el head y el footer de WordPress para que puedan ir sus funcionalidades básicas
+wp_head();
+wp_footer();// Llama al header.php o al footer.php
+get_header();
+wp_footer();// Saca el título que se ha asignado en el back
+the_title();// Saca el contenido que se ha asignado en el back
+echo the_content();?>
