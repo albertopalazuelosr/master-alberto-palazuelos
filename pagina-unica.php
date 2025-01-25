09052244bc6d4e4e96ec8037803a78bcb9238685
albertopalazuelosr <albertopalazuelosr@gmail.com>
2025-01-25 08:14:21 +0100

    Cambios en página única

---
+++ pagina-unica.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <title>Title d  la página</title>
+    <meta name="description" content="Metadescription de la página">
+    <meta name="google-bot" content="all, max-snippet:25, max-video-preview:0, unavailable after: [2033-09-27]">
+    <meta http-equiv="refresh" content="30, url=https://www.google.com/">
+</head>
+<body>
+    <header>
+        <nav></nav>
+    </header>
+    <section>
+        <h1>Título de la página</h1>
+        <div data-nosnippet>Este texto no aparecería en los resultados</div>
+    </section>
+    <footer>
+    </footer>
+</body>
